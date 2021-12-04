96555b6732a0278228a70ab2b4ed598debffd023
Raja Soundaramourty <rajasoun@icloud.com>
2021-12-04 16:29:16 +0530

    feat(tooling): doc update

--- provision/scripts/cli/ansible.sh
+++ provision/scripts/cli/ansible.sh
@@ -17,6 +17,7 @@ function ansible_manager() {
     configure_control_center
     ;;
   users)
+    is_vm && raise_error "user configuration can't run on host"
     echo "Configure Users..."
     configure_users
     echo -e "${GREEN}\nNext  SSH to Control Center -> ./assist.sh login control-center $USER \n${NC}"
